98a302388badbe1558388746d099af1f14526c0f
Sevastian Dvortsov <fane28@mail.ru>
2021-11-19 18:09:52 +0300

    fix: refactor photos js functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,23 @@
-const firstScreenButton = document.querySelector('.first-screen__button');
-const about = document.querySelector('.about');
+const container = document.querySelector('.page');
+const firstScreenButton = container.querySelector('.first-screen__button');
+const about = container.querySelector('.about');
+const photosListItemsArr = container.querySelectorAll('.photos__list-item');
+const dotsScrollDotArr = container.querySelectorAll('.dots-scroll__dot');
+const formDonutsBtnDonutSum = container.querySelectorAll('.form-donuts__button-donut-sum');
+const popupBtnClose = container.querySelector('.popup__button-close');
+const popupTypeDonuts = container.querySelector('.popup_type_donuts');
+const headerSupportBtn = container.querySelector('.header__support-btn');
+const popupNavDonateBtn = container.querySelector('.popup-nav__donate-btn');
+const popupNav = container.querySelector('.popup-nav');
+const popupCities = container.querySelector('.popup-cities');
+const headerBurger = container.querySelector('.header__burger');
+const popupNavCityChooseBtn = container.querySelector('.popup-nav__city-choose-btn');
+const popupCitiesBackspaceBtn = container.querySelector('.popup-cities__backspace-btn');
+const popupCitiesCityArr = container.querySelectorAll('.popup-cities__city');
+const popupNavCity =container.querySelector('.popup-nav__city');
+const headerLocation = container.querySelector('.header__location');
+const popupCitiesCityChooseBtnArr = container.querySelectorAll('.popup-cities__city-choose-btn');
+const popupCitiesCityMarkerArr = container.querySelectorAll('.popup-cities__city-marker');
 
 function firstScreenButtonClick() {
   about.scrollIntoView({behavior: "smooth", block: "start"});
@@ -7,48 +25,41 @@ function firstScreenButtonClick() {
 
 firstScreenButton.addEventListener('click', firstScreenButtonClick);
 
-/*Определяем массив картинок*/
-const photosListItemsArr = document.querySelectorAll('.photos__list-item');
-
-/*Определяем масcив точек переключения*/
-const dotsScrollDotArr = document.querySelectorAll('.dots-scroll__dot');
-
-/*Настраиваем переключение картинок через нажатие на картинку*/
-photosListItemsArr.forEach(function (currentValue, index,currentArray ){
-  currentArray[index].addEventListener('click',  function(){
-    if (index === currentArray.length - 1 ){
-      currentArray[index].classList.toggle('photos__list-item_active');
-      currentArray[0].classList.toggle('photos__list-item_active');
-      dotsScrollDotArr[index].classList.toggle('dots-scroll__dot_active');
-      dotsScrollDotArr[0].classList.toggle('dots-scroll__dot_active');
+photosListItemsArr.forEach(function (currentValue, index){
+  currentValue.addEventListener('click',  function(){
+    if (index === photosListItemsArr.length - 1 ){
+      currentValue.classList.remove('photos__list-item_active');
+      photosListItemsArr[0].classList.add('photos__list-item_active');
+      dotsScrollDotArr[index].classList.remove('dots-scroll__dot_active');
+      dotsScrollDotArr[0].classList.add('dots-scroll__dot_active');
     }else{
-      currentArray[index].classList.toggle('photos__list-item_active');
-      currentArray[index + 1].classList.toggle('photos__list-item_active');
-      dotsScrollDotArr[index].classList.toggle('dots-scroll__dot_active');
-      dotsScrollDotArr[index + 1].classList.toggle('dots-scroll__dot_active');
+      currentValue.classList.remove('photos__list-item_active');
+      photosListItemsArr[index + 1].classList.add('photos__list-item_active');
+      dotsScrollDotArr[index].classList.remove('dots-scroll__dot_active');
+      dotsScrollDotArr[index + 1].classList.add('dots-scroll__dot_active');
     }
   });
 });
 
+
 /*Настраиваем переключение картинок через нажатие на точку*/
-dotsScrollDotArr.forEach(function (currentValue, index,currentArray ) {
-  currentArray[index].addEventListener('click', function () {
-    document.querySelector('.dots-scroll__dot_active').classList.toggle('dots-scroll__dot_active');
-    document.querySelector('.photos__list-item_active').classList.toggle('photos__list-item_active');
-    currentArray[index].classList.toggle('dots-scroll__dot_active');
-    photosListItemsArr[index].classList.toggle('photos__list-item_active');
+dotsScrollDotArr.forEach(function (currentValue, index) {
+  currentValue.addEventListener('click', function () {
+    container.querySelector('.dots-scroll__dot_active').classList.remove('dots-scroll__dot_active');
+    container.querySelector('.photos__list-item_active').classList.remove('photos__list-item_active');
+    currentValue.classList.add('dots-scroll__dot_active');
+    photosListItemsArr[index].classList.add('photos__list-item_active');
   });
 })
 
 /*Настраиваем закраску кнопок с суммами*/
-const formDonutsBtnDonutSum = document.querySelectorAll('.form-donuts__button-donut-sum');
 
 formDonutsBtnDonutSum.forEach(function (currentValue, index,currentArray ) {
   currentArray[index].addEventListener('click', function () {
-    if (document.querySelector('.form-donuts__button-donut-sum_checked') === null){
+    if (container.querySelector('.form-donuts__button-donut-sum_checked') === null){
       currentArray[index].classList.toggle('form-donuts__button-donut-sum_checked');
     }else {
-      document.querySelector('.form-donuts__button-donut-sum_checked').classList.toggle('form-donuts__button-donut-sum_checked');
+      container.querySelector('.form-donuts__button-donut-sum_checked').classList.toggle('form-donuts__button-donut-sum_checked');
       currentArray[index].classList.toggle('form-donuts__button-donut-sum_checked');
     }
   });
@@ -69,10 +80,6 @@ function popupToggle(popupObject){
 
 /*Вешаем лиссенеры на элементы открытия и закрытия popups*/
 /*Popup с формой пожертвования*/
-const popupBtnClose = document.querySelector('.popup__button-close');
-const popupTypeDonuts = document.querySelector('.popup_type_donuts');
-const headerSupportBtn = document.querySelector('.header__support-btn');
-const popupNavDonateBtn = document.querySelector('.popup-nav__donate-btn');
 
 headerSupportBtn.addEventListener('click', function (){
   popupOpen(popupTypeDonuts);
@@ -87,14 +94,6 @@ popupBtnClose.addEventListener('click', function (){
 });
 
 /*Popup с меню и городами*/
-const popupNav = document.querySelector('.popup-nav');
-const popupCities = document.querySelector('.popup-cities');
-const headerBurger = document.querySelector('.header__burger');
-const popupNavCityChooseBtn = document.querySelector('.popup-nav__city-choose-btn');
-const popupCitiesBackspaceBtn = document.querySelector('.popup-cities__backspace-btn');
-const popupCitiesCityArr = document.querySelectorAll('.popup-cities__city');
-const popupNavCity =document.querySelector('.popup-nav__city');
-const headerLocation = document.querySelector('.header__location');
 
 headerBurger.addEventListener('click', function (){
   popupNav.classList.toggle('popup-nav_opened');
@@ -112,15 +111,13 @@ headerLocation.addEventListener('click', function (){
   popupCities.classList.toggle('popup-cities_opened');
 });
 
-const popupCitiesCityChooseBtnArr = document.querySelectorAll('.popup-cities__city-choose-btn');
-const popupCitiesCityMarkerArr = document.querySelectorAll('.popup-cities__city-marker');
 
 popupCitiesCityChooseBtnArr.forEach(function (currentValue, index,currentArray ){
   currentArray[index].addEventListener('click',  function(){
-    if (document.querySelector('.popup-cities__city-marker_active') === null){
+    if (container.querySelector('.popup-cities__city-marker_active') === null){
       popupCitiesCityMarkerArr[index].classList.toggle('popup-cities__city-marker_active');
     }else {
-      document.querySelector('.popup-cities__city-marker_active').classList.toggle('popup-cities__city-marker_active');
+      container.querySelector('.popup-cities__city-marker_active').classList.toggle('popup-cities__city-marker_active');
       popupCitiesCityMarkerArr[index].classList.toggle('popup-cities__city-marker_active');
     }
     popupNavCity.textContent = popupCitiesCityArr[index].textContent;
