2f00e0312bc4f6c00b2de2c19a8a2032004557fb
Sevastian Dvortsov <fane28@mail.ru>
2021-11-22 12:29:51 +0300

    feat: add swiper into photo block

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 const firstScreenButton = document.querySelector('.first-screen__button');
 const about = document.querySelector('.about');
+const swiper = document.querySelector('.swiper');
+
+
 
 function firstScreenButtonClick() {
   about.scrollIntoView({behavior: "smooth", block: "start"});
@@ -7,38 +10,33 @@ function firstScreenButtonClick() {
 
 firstScreenButton.addEventListener('click', firstScreenButtonClick);
 
-/*Определяем массив картинок*/
-const photosListItemsArr = document.querySelectorAll('.photos__list-item');
+if (document.documentElement.clientWidth < 400) {
+  const mySwiper = new Swiper('.photos', {
+    loop: true,
 
-/*Определяем масcив точек переключения*/
-const dotsScrollDotArr = document.querySelectorAll('.dots-scroll__dot');
-
-/*Настраиваем переключение картинок через нажатие на картинку*/
-photosListItemsArr.forEach(function (currentValue, index,currentArray ){
-  currentArray[index].addEventListener('click',  function(){
-    if (index === currentArray.length - 1 ){
-      currentArray[index].classList.toggle('photos__list-item_active');
-      currentArray[0].classList.toggle('photos__list-item_active');
-      dotsScrollDotArr[index].classList.toggle('dots-scroll__dot_active');
-      dotsScrollDotArr[0].classList.toggle('dots-scroll__dot_active');
-    }else{
-      currentArray[index].classList.toggle('photos__list-item_active');
-      currentArray[index + 1].classList.toggle('photos__list-item_active');
-      dotsScrollDotArr[index].classList.toggle('dots-scroll__dot_active');
-      dotsScrollDotArr[index + 1].classList.toggle('dots-scroll__dot_active');
+    pagination: {
+        el: '.swiper-pagination',
+        clicable: true,
+    },
+    breakpoints: {
+      400: {
+        enabled: false
+      }
     }
-  });
-});
 
-/*Настраиваем переключение картинок через нажатие на точку*/
-dotsScrollDotArr.forEach(function (currentValue, index,currentArray ) {
-  currentArray[index].addEventListener('click', function () {
-    document.querySelector('.dots-scroll__dot_active').classList.toggle('dots-scroll__dot_active');
-    document.querySelector('.photos__list-item_active').classList.toggle('photos__list-item_active');
-    currentArray[index].classList.toggle('dots-scroll__dot_active');
-    photosListItemsArr[index].classList.toggle('photos__list-item_active');
   });
-})
+}
+
+if (document.documentElement.clientWidth > 400) {
+  const swiperSlide = swiper.querySelectorAll('.swiper-slide');
+  const swiperWrapper = swiper.querySelector('.swiper-wrapper');
+  swiperSlide.forEach((currentValue) => {
+    currentValue.classList.remove('swiper-slide');
+  })
+  swiperWrapper.classList.remove('swiper-wrapper');
+  swiper.classList.remove('swiper');
+
+}
 
 /*Настраиваем закраску кнопок с суммами*/
 const formDonutsBtnDonutSum = document.querySelectorAll('.form-donuts__button-donut-sum');
